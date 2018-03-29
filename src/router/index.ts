0f85c0c2299b1ee0b74f0ef8d5de3e465e006a76
Bennett Hardwick <bennetthardwick@icloud.com>
2018-03-29 18:40:48 +1000

    Remove hello world route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,4 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!');
-});
-
 module.exports = router;
